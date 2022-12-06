dda4c1613a9a8d1055c2020f3385293ed946f59e
Cédric Chevalier <cedric.chevalier@cea.fr>
2022-12-06 11:33:13 +0100

    Support of almalinux based image

--- spack/install_spack_deps.sh
+++ spack/install_spack_deps.sh
@@ -1,7 +1,24 @@
 #! /usr/bin/bash
 
+install_apt() {
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y build-essential ca-certificates coreutils curl environment-modules gfortran git gpg lsb-release python3 python3-distutils python3-venv unzip zip curl
 apt clean
 rm -rf /var/lib/apt/lists/*
+}
+
+install_dnf()
+{
+    dnf -y update
+    dnf -y group install "Development Tools"
+    dnf -y install curl gnupg2 python3 python3-pip python3-setuptools
+    dnf -y install bash
+    dnf clean all
+}
+
+if [ "$(type apt)" ]; then
+    install_apt;
+elif [ "$(type dnf)" ]; then
+    install_dnf;
+fi
